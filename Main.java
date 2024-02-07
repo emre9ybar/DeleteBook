af93faa93dc6dab29e3dd45862b9946777bd4750
emre aybar <aybaremre5@gmail.com>
2024-02-07 23:31:59 +0300

    DeleteBooks

---
+++ Main.java
@@ -0,0 +1,38 @@
+package newExample;
+
+public class Main {
+    public static void deleteBook(String ISBN) {
+        int findIndex = -1;
+        if (bookQuantity == 0) {
+            System.out.println("Kütühanede kitap sayısı 0'dır.");
+        }
+        for (int i = 0; i < bookQuantity; i++) {
+            if (books[i][3].equals(ISBN)) {
+                findIndex = i;
+                break;
+            }
+        }
+        if (findIndex != -1) {
+            for (int i = findIndex; i < bookQuantity - 1; i++) {
+                books[i] = books[i + 1];
+            }
+            bookQuantity--;
+            String[][] newBooks = new String[books.length][4];
+            for (int i = 0; i < books.length; i++) {
+                for (int j = 0; j < books[i].length; j++) {
+                    newBooks[i][j] = books[i][j];
+                }
+            }
+            books = newBooks;
+            System.out.println("Kitap Silinmiştir.");
+        } else {
+            System.out.println("Silmek isteğiniz kitap bulunmamaktadır.");
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
